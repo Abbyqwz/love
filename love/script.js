cba6c9cd5fedf23de3801ca778963a58c16d198c
Abbyqwz <abbyduranp@gmail.com>
2024-07-29 20:01:16 -0600

    Add files via upload

---
+++ love/script.js
@@ -0,0 +1,8 @@
+//script.js
+function mostrarMensaje(){
+    const mensaje = ("Te quiero demasiado May, eres tan hermosa como la luna.");
+    const mensajeDiv =
+    document.getElementById('mensaje');
+    mensajeDiv.textContent = mensaje;
+    mensajeDiv.style.display= 'block';
+}
